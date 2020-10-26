0b660e35b873cee70381cf39455015dd48324622
Rainboom7 <palagutin.alexander@yandex.ru>
2020-10-26 11:33:36 +0300

    edited options

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ def edadeal_parser():
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.binary_location = GOOGLE_CHROME_BIN
     driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=chrome_options)
 
