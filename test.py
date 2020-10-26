184bedb376686edb12cd8cb1d42a6fd8e7c97070
Rainboom7 <palagutin.alexander@yandex.ru>
2020-10-26 11:38:38 +0300

    added headless option

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ def edadeal_parser():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.binary_location = GOOGLE_CHROME_BIN
