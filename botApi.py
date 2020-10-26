ef8de0c8dc9f056c510c365e1d19fa6d0bc7e13e
Saoqq <wfmfe@ya.ru>
2020-10-27 00:06:11 +0300

    Extract parser functions to separate file

--- botApi.py
+++ botApi.py
@@ -4,7 +4,9 @@ import requests
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-from test import *
+
+from pivoParser import parsePyaterochka, parseMagnit, parseKb
+from pivoParserSelenium import *
 
 
 def uploadImages(images, offset, vk_session):
@@ -104,37 +106,6 @@ def createPollMessage(time_from, time_to, chat_id):
         poll_data += time_data + "\n"
         i += 0.5
     return poll_data
-
-
-def parsePyaterochka():
-    response = requests.get(
-        "https://skidkaonline.ru/apiv3/products/?limit=30&offset=0&pcategories_ids=414&shop_id=9&city_id=42")
-    products = json.loads(response.text)["products"]
-    images = []
-    for product in products:
-        images.append(product['imagefull']['src'])
-    return images
-
-
-def parseMagnit():
-    response = requests.get(
-        "https://skidkaonline.ru/apiv3/products/?limit=30&offset=0&pcategories_ids=414&shop_id=2&city_id=42")
-    products = json.loads(response.text)["products"]
-    images = []
-    for product in products:
-        images.append(product['imagefull']['src'])
-    return images
-
-
-def parseKb():
-    beers = {}
-    response = requests.get(
-        "https://skidkaonline.ru/apiv3/products/?limit=30&offset=0&pcategories_ids=414&shop_id=100&city_id=42")
-    products = json.loads(response.text)["products"]
-    images = []
-    for product in products:
-        beers.update({product['name']: product['priceafter']})
-    return beers
 
 
 def getVoteKeyboard(chat_id):
